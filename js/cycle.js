da67b43e9aa0eb6f06c97fe3b1610776c75c700a
WESLEY- BL4CKS1D3 SYSTEM <81283805+bl4cks1d3@users.noreply.github.com>
2025-03-11 09:35:33 -0300

    clean input email

--- js/cycle.js
+++ js/cycle.js
@@ -169,7 +169,7 @@ function addDays(date, days) {
 	xhr.onload = function() {
 		if (xhr.status >= 200 && xhr.status < 300) {
 			alert("Obrigada pela sua participação")
-			email = "";
+			document.getElementById('s-email').value = "";
 		} 
 	};
 	xhr.send();
